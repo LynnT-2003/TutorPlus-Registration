130eb09d3a822aeb4af2519d7f833ee5fc903937
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-03-27 23:16:37 +0700

    created login skeleton

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,75 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
 export default function Login() {
-  const [userId, setUserId] = useState("");
+  const [adminDb, setAdminDb] = useState([]);
+  const [currentAdminID, setCurrentAdminID] = useState([]);
+
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggedInAdmin, setIsLoggedInAdmin] = useState("");
+
   const router = useRouter();
-  const { data: session } = useSession();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/tutorPlus/admins")
+      .then((response) => {
+        setAdminDb(response.data);
+        console.log(adminDb);
+        alert("Successfully fetched Admin data");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const result = await signIn("credentials", {
-      redirect: false,
-      userId,
-      password,
-    });
-    if (result.error) {
-      console.error(result.error);
+    const admin = adminDb.find((a) => a.adminId === username);
+    if (admin && password === admin.adminName) {
+      setIsLoggedInAdmin(true);
+      setCurrentAdminID(username);
+      router.push("/admin");
     } else {
-      router.push(`/${result.role}/${result.id}`);
+      setIsLoggedInAdmin(false);
+      alert("Incorrect username or password");
+      alert({ adminDb });
+      console.table(adminDb);
+      console.log(username);
     }
   };
 
-  if (session) {
-    router.push(`/${session.role}/${session.id}`);
-    return null;
-  }
-
   return (
     <>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="userId">User ID:</label>
-          <input
-            type="text"
-            id="userId"
-            name="userId"
-            value={userId}
-            onChange={(event) => setUserId(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      <div className="admin-login">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">User ID:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </>
   );
 }
