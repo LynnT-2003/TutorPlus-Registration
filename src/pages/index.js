bae034301ba0453ea9a973927b80b5646a5bc8da
acm1995 <aungchammyae95@gmail.com>
2023-03-29 15:09:20 +0700

    minor CSS change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import { useRouter } from "next/router";
-import Image from 'next/legacy/image';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import Image from "next/legacy/image";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function Login() {
   const router = useRouter();
@@ -23,57 +22,59 @@ export default function Login() {
   };
 
   const goHome = () => {
-    router.push('/');
+    router.push("/");
   };
-  
+
   const goAdmin = () => {
-    router.push('/login/admin');
+    router.push("/login/admin");
   };
-  
+
   const goTutor = () => {
-    router.push('/login/tutor');
+    router.push("/login/tutor");
   };
-  
+
   const goStudent = () => {
-    router.push('/login/student');
+    router.push("/login/student");
   };
 
   const landingText = {
-    fontSize: '5rem',
-    fontWeight: 'bold',
-    lineHeight: '1.2',
-    color: '#3D9FBA',
-    marginTop: '10rem',
-    marginBottom: '5rem',
+    fontSize: "5rem",
+    fontWeight: "bold",
+    lineHeight: "1.2",
+    color: "#3D9FBA",
+    marginTop: "10rem",
+    marginBottom: "5rem",
   };
 
   const homeText = {
-    fontSize: '1.5rem',
-    lineHeight: '1.2',
-    color: '#333',
-    marginTop: '5',
-    marginBottom: '5rem',
+    fontSize: "1.5rem",
+    lineHeight: "1.2",
+    color: "#333",
+    marginTop: "5",
+    marginBottom: "5rem",
   };
 
   return (
     <>
       <div className="BGandDiv">
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand onClick={goHome}>TutorPlus</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link onClick={goAdmin}>Admin</Nav.Link>
-            <Nav.Link onClick={goTutor}>Tutor</Nav.Link>
-            <Nav.Link onClick={goStudent}>Student</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-        <div style={{
-          zIndex: -1,
-          position: "fixed",
-          width: "100vw",
-          height: "100vh"
-        }}>
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand onClick={goHome}>TutorPlus</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link onClick={goAdmin}>Admin</Nav.Link>
+              <Nav.Link onClick={goTutor}>Tutor</Nav.Link>
+              <Nav.Link onClick={goStudent}>Student</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <div
+          style={{
+            zIndex: -1,
+            position: "fixed",
+            width: "100vw",
+            height: "100vh",
+          }}
+        >
           {/* <Image
             src="/images/background.jpg"
             alt="Background"
@@ -84,19 +85,37 @@ export default function Login() {
         </div>
       </div>
 
-      <Container style={landingText}>
-        Welcome to TutorPlus
-      </Container>
+      <Container style={landingText}>Welcome to TutorPlus</Container>
 
       <Container style={homeText}>
-      "Streamline your tutoring experience with VMS Tutoring."
+        "Streamline your tutoring experience with VMS Tutoring."
       </Container>
 
-
-      <Container>
-        <Button variant="outline-secondary" size='lg' style={{margin: '20px'}} onClick={adminLogin}>Login as Admin</Button>
-        <Button variant="outline-secondary" size='lg' style={{margin: '20px'}} onClick={tutorLogin}>Login as Tutor</Button>
-        <Button variant="outline-secondary" size='lg' style={{margin: '20px'}} onClick={studentLogin}>Login as Student</Button>
+      <Container style={{ marginTop: "20rem" }}>
+        <Button
+          variant="outline-secondary"
+          size="lg"
+          style={{ margin: "20px" }}
+          onClick={adminLogin}
+        >
+          Login as Admin
+        </Button>
+        <Button
+          variant="outline-secondary"
+          size="lg"
+          style={{ margin: "20px" }}
+          onClick={tutorLogin}
+        >
+          Login as Tutor
+        </Button>
+        <Button
+          variant="outline-secondary"
+          size="lg"
+          style={{ margin: "20px" }}
+          onClick={studentLogin}
+        >
+          Login as Student
+        </Button>
       </Container>
     </>
   );
